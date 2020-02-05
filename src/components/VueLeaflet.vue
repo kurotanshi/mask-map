77ffd18a76c2ef93d32d12f0ea5da68d4911e65f
Kuro Hsu <kurotanshi@gmail.com>
2020-02-05 22:48:17 +0800

    change icon

--- src/components/VueLeaflet.vue
+++ src/components/VueLeaflet.vue
@@ -2,14 +2,18 @@
   <div class="vue-leaflet">
     <div class="map">
 
-      <l-map :zoom="zoom" :center="center" ref="myMap">
+      <l-map :zoom="zoom" :center="center" :options="{ zoomControl: false }" ref="myMap">
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+
+        <l-marker :lat-lng="center" :icon="icon" ref='hereMarker'></l-marker>
+
         <v-marker-cluster>
-          <l-marker v-for="s in stores" :key="s.id" :lat-lng="getCoods(s.x, s.y)" >
+          <l-marker v-for="s in stores" :key="s.id" :lat-lng="getCoods(s.x, s.y)">
             <l-popup :content="getPopup(s)"></l-popup>
           </l-marker>
         </v-marker-cluster>
 
+        <!-- <l-geo-json :geojson="geoJson" :options="geoJsonOptions"></l-geo-json> -->
       </l-map>
     </div>
   </div>
@@ -23,6 +27,7 @@ import {
   LTileLayer,
   LMarker,
   LPopup
+  // LGeoJson,
 } from 'vue2-leaflet'
 
 var L = window.L
@@ -34,14 +39,23 @@ export default {
     LMap,
     LTileLayer,
     LMarker,
+    // LGeoJson,
     LPopup
   },
   data () {
     return {
       stores: [],
-      zoom: 11,
+      zoom: 16,
       center: L.latLng(25.0677505, 121.5470599),
       token: '',
+      icon: L.icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      }),
       url: 'https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
     }
@@ -67,21 +81,31 @@ export default {
       `
     },
     renderMap () {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        const p = pos.coords
-        this.center = L.latLng(p.latitude, p.longitude)
-        this.zoom = 14
-      })
-
       fetch('./med-stores.json')
         .then(res => res.json())
         .then(jsonData => {
+          // console.log(jsonData[0])
           this.stores = jsonData
         })
     }
   },
   mounted () {
     this.renderMap()
+
+    this.$nextTick(() => {
+      const map = this.$refs.myMap.mapObject
+      L.control.zoom({ position: 'bottomright' }).addTo(map)
+
+      navigator.geolocation.getCurrentPosition((pos) => {
+        const p = pos.coords
+        this.center = L.latLng(p.latitude, p.longitude)
+        this.$refs.hereMarker.mapObject.bindTooltip('You', {
+          offset: [0, -36],
+          permanent: true,
+          direction: 'top'
+        })
+      })
+    })
   }
 }
 </script>
